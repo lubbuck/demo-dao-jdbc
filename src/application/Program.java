2f4d37a5804cf827ae0b3143fa3aa4a1f2367e15
Antônio Luís <antonioluisp97@gmail.com>
2024-01-05 20:09:56 -0300

    feat: findByDepartment no SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -9,10 +12,17 @@ public class Program {
 	public static void main(String[] args) {
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
+
 		System.out.println("==== Teste 1 FindById ====");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
+
+		System.out.println("==== Teste 2 FindByDepartment ====");
+		List<Seller> sellers = sellerDao.findByDepartment(new Department(2, null));
+
+		for (Seller sellerTemp : sellers) {
+			System.out.println(sellerTemp);
+		}
 
 	}
 }
