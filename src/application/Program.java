09e4bc00df56a1213746b2f121bf5301a7d36275
Antônio Luís <antonioluisp97@gmail.com>
2024-01-04 21:58:15 -0300

    fix: reutilizando metodo de criacao do seller e department no sellerdaojdbc

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,7 +9,9 @@ public class Program {
 	public static void main(String[] args) {
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		Seller seller = sellerDao.findById(8);
+		
+		System.out.println("==== Teste 1 FindById ====");
+		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 
 	}
